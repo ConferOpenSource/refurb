b984b3fe91dbc4e6573e67977dcd978e899c69dd
Dan Fithian <danielf@vitalbio.com>
2023-12-04 11:14:16 -0500

    Bump to 3.0.3

--- default.nix
+++ default.nix
@@ -3,14 +3,14 @@
 
 with builtins;
 let
-  haskellNix = import (builtins.fetchTarball "https://github.com/input-output-hk/haskell.nix/archive/2f3ec7bbf2e790122ca276a4ab038bf2749c6c93.tar.gz") {};
+  haskellNix = import (builtins.fetchTarball "https://github.com/input-output-hk/haskell.nix/archive/a9efc0ae3a607eaebc6e841ca3960134e9034077.tar.gz") {};
   pkgs = import nixpkgsSrc haskellNix.nixpkgsArgs;
   project = pkgs.haskell-nix.cabalProject {
     name = "refurb";
     src = ./.;
     cabalProject = readFile ./cabal.project;
     compiler-nix-name = "ghc925";
-    index-state = "2022-12-01T00:00:00Z";
+    index-state = "2023-12-01T00:00:00Z";
     modules = [
       {
         packages.postgresql-libpq = {
